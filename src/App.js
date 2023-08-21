a3b267660ace5fa4697ec5ad5b2b8b2e76b1894b
AkashDY <sky.deep.1997@gmail.com>
2023-08-21 15:36:55 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./components/pages/Home";
+import AuthPage from "./components/Auth/AuthPage";
+import Profile from "./components/Profile/Profile";
+import PrivateRoutes from "./utils/PrivateRoutes";
+import ForgotPassword from "./components/Auth/ForgotPassword";
+const App = () => {
+  // const auth = localStorage.getItem("token");
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<AuthPage />} />
+        <Route
+          path="/profile"
+          element={
+            <PrivateRoutes>
+              <Profile />
+            </PrivateRoutes>
+          }
+        />
+        <Route
+          path="/home"
+          element={
+            <PrivateRoutes>
+              <Home />
+            </PrivateRoutes>
+          }
+        />
+        <Route path="/forgot" element={<ForgotPassword />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default App;
---
+++ src/components/Auth/ForgotPassword.jsx
@@ -0,0 +1,40 @@
+import React, { useRef } from "react";
+import { useNavigate } from "react-router-dom";
+import classes from "./AuthPage.module.css";
+function ForgotPassword() {
+  const enteredEmail = useRef();
+  const navigate = useNavigate();
+  async function submitHendler() {
+    const res = await fetch(
+      "https://identitytoolkit.googleapis.com/v1/accounts:sendOobCode?key=AIzaSyCU5htJbsi7PPgASZ7_N5MjWr0esR_8l-Y",
+      {
+        method: "POST",
+        body: JSON.stringify({
+          requestType: "PASSWORD_RESET",
+          email: enteredEmail.current.value,
+        }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
+    if (res.ok) {
+      alert("check your mail box");
+      navigate("/");
+    }
+  }
+  return (
+    <div className={classes.signup}>
+      <div className={classes.form}>
+        <h1>Forgot password</h1>
+        <input ref={enteredEmail} type="email" placeholder="Enter email id" />
+        <button className={classes.signupButton} onClick={submitHendler}>
+          Submit
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default ForgotPassword;
