74fcc8c545dfaf6173f73006022339b75628273b
AkashDY <89009592+AkashDY@users.noreply.github.com>
2023-12-04 15:41:05 +0530

    Update EditExpense.jsx

--- src/components/pages/EditExpense.jsx
+++ src/components/pages/EditExpense.jsx
@@ -11,7 +11,7 @@ function EditExpense(props) {
   const priceRef = useRef();
   const url =
     // "https://expense-tracker-69a2b-default-rtdb.asia-southeast1.firebasedatabase.app";
-    "https://expensetracker-d8f76-default-rtdb.firebaseio.com";
+    "https://expense-tracker-app-23a2c-default-rtdb.firebaseio.com/&{email}/expenses.json";
     const email = localStorage.getItem("email").replace(/[@.]/g, "");
 
   async function editHandler(e) {
